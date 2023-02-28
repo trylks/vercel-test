2b5c982afd9a5231e95de7920fe8bcb0174048f3
trylks <1286855+trylks@users.noreply.github.com>
2023-02-28 17:18:13 +0000

    javascript works acceptably

--- pages/api/getTitle.js
+++ pages/api/getTitle.js
@@ -6,5 +6,6 @@ export default function handler(req, res) {
   fetch(req.body.url) // fetch the url
     .then((r) => r.text()) // get the text from the response
     .then((html) => res.status(200).json( // send our response
-      cheerio.load(html)('title').text())) // with the text of the title of the html retrieved
+      cheerio.load(html)('title').text()))// with the text of the title of the html retrieved
+    .catch((_) => res.status(400).json('Bad URL'))
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function Home() {
-  const [url, setUrl] = useState('');
   const [title, setTitle] = useState('');
-
-  const handleUrlChange = (event) => { setUrl(event.target.value) };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    return fetch('/api/getTitle', {
+  const [url, setURL] = useState('');
+  useEffect(() => {
+    setURL('http' + window.location.href.split('http').at(-1));
+    fetch('/api/getTitle', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ url })
-    }).then((r) => r.json())
-      .then(setTitle);
-  };
+    }).then((r) => r.json()).then(setTitle)
+  });
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter a URL:
-          <input type="text" value={url} onChange={handleUrlChange} />
-        </label>
-        <button type="submit">Get Title</button>
-      </form>
-      <h1>{title}</h1>
-    </div>
-  );
+  return <h1>{title}</h1>;
 }
