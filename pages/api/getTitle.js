22d5c7d0ed7cb7c432711e97f77c67ed75768d3e
trylks <1286855+trylks@users.noreply.github.com>
2023-03-03 14:12:28 +0000

    simplifying before complicating

--- pages/api/getTitle.js
+++ pages/api/getTitle.js
@@ -2,9 +2,8 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 export default function handler(req, res) {
-  fetch(req.body.url) // fetch the url
+  fetch(req.query.url) // fetch the url
     .then((r) => r.text()) // get the text from the response
-    .then((html) => res.status(200).json( // send our response
-      cheerio.load(html)('title').text()))// with the text of the title of the html retrieved
+    .then((html) => res.status(200).end(cheerio.load(html)('title').text())) // return the html title text
     .catch((_) => res.status(400).json('Bad URL'))
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,9 @@ import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [title, setTitle] = useState('');
-  const [url, setURL] = useState('');
   useEffect(() => {
-    setURL('http' + window.location.href.split('http').at(-1));
-    fetch('/api/getTitle', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url })
-    }).then((r) => r.json()).then(setTitle)
+    const url = encodeURIComponent(new URLSearchParams(window.location.search).get('url'));
+    fetch('/api/getTitle?url=' + url).then((r) => r.text()).then(setTitle)
   });
 
   return <h1>{title}</h1>;
